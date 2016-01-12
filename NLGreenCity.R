178d139d15fc419cfc75ea2c0ceb5bd6dbe784f8
Slenders <lieven.slenders@wur.nl>
2016-01-12 18:30:07 +0100

    All works for January Problem for other months is:
    1- defining the month r[32]
    2- consistency of the header of the month colum
    r[50 and further]

--- NLGreenCity.R
+++ NLGreenCity.R
@@ -12,6 +12,7 @@ createFolders <- function() {
   dir.create('output/') -> ofolder
 }
 
+## Preprocessing ----------------------------------------
 # Download 
 source('R/downloadData.R')
 downloadData("https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data/MODIS.zip", 
@@ -25,58 +26,46 @@ library(sp)
 library(raster)
 MODIS <- stack("data/MOD13A3.A2014001.h18v03.005.grd")
 
+## Calculations --------------------------------------------
 
-## Select January
-          ## Both ways work to select a column from a raster package
-MODIS_jan <- raster(MODIS, 1)
-#MODIS_jan <- MODIS@layers[[1]]
-#plot(January)
+## Select Month = January on MODIS
+MODIS_month <- raster(MODIS, 1)
+# MODIS_month <- MODIS@layers[[1]]    ## Both ways work to select a column from a raster package
+# plot(MODIS_month)
+
 
 ## Import municipality mask & Apply CRS 
+# for country =NLD ;level of detail=2
 municipality_mask <- spTransform(raster::getData('GADM',country= "NLD", level= 2), CRS(proj4string(MODIS)))
 
-## Extracting NDVI for defined mask          
-NDVI_NL <- extract(MODIS_jan, municipality_mask, sp=TRUE, df=TRUE, fun=mean) ## df=TRUE i.e. return as a data.frame
 
+## Extracting NDVI; calculate mean; for defined mask ---------------------------------------------
+paste(" takes some time ...")
+raw_NDVI_NL <- extract(MODIS_month, 
+                   municipality_mask,
+                   sp=TRUE,
+                   df=TRUE,
+                   fun=mean) ## df=TRUE i.e. return as a data.frame
+
+str(raw_NDVI_NL@data) # to check month-colum header; NDVI-name
+
+## Remove NA values ------! still month in the column header!! ???
+NDVI_NL <- raw_NDVI_NL[!is.na(raw_NDVI_NL$January),] ## remove rows with NA
+
+# answer computing --------------------------------------------------------------------------------
+
+
+df <- NDVI_NL@data # has municip name as " df$NAME_2"  ; NDVI as " month name" 
+
+# xplore data
 str(NDVI_NL@data)
-
-data <- NDVI_NL@data
-
-df$NAME_2
-max(df$January)
-t <-cbind(df$NAME_2,df$January)
-
-# # Combine the newly created dataframe to the description column 
-colMax <- function(data)
-
-sapply(data, max, na.rm = TRUE)
+df$NAME_2           # municipalityname
+df$January          # NDVI values
 
 
 
-
-##Remove NA values
-NDVI_NL_no_NA <- NDVI_NL[!is.na(NDVI_NL$January),] ## remove rows with NA
-# max value 
-max(NDVI_NL_no_NA$January)
-
-
-
-DF <- list("NDVI" = NDVI_NL_no_NA$January, "Municipalities" = municipality_mask)
- %in% NDVI_NL_no_NA$January
-
-#
-
-
-
-
-
-
-# class(NDVI_NL)  ## computation efficiency: Is there more data than shown? 
-
-
-
-
-
-
+# select max NDVI ;  and display corresponding Municipality name
+answer <- df[which(df$January == max(df$January, na.rm = TRUE)), ]$NAME_2
+answer
 
 
