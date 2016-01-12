a476f9e9f3b4c96f9e3582aeb20a9707d2f9761d
Slenders <lieven.slenders@wur.nl>
2016-01-12 14:28:28 +0100

    hgfd

--- NLGreenCity.R
+++ NLGreenCity.R
@@ -4,12 +4,12 @@
 ## January 12-1-2015
 ## Lesson 7
 # -------------------------------------------------------
-getwd()
-setwd("D:/workspace/geoscripting/lesson7")
+
+## Create a working directory to maintain input and output data
 createFolders <- function() {
-  dir.create('data/')
-  dir.create('R/')
-  dir.create('output/')
+  dir.create('data/') -> ifolder
+  dir.create('R/') 
+  dir.create('output/') -> ofolder
 }
 
 # Download 
@@ -20,4 +20,18 @@ downloadData("https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data
 source('R/unzipFile.R')
 unzipFile('data/MODIS', 'data')
 
+## Reading the grd file
+library(sp)
+library(raster)
 
+MODIS <- stack("data/MOD13A3.A2014001.h18v03.005.grd")
+
+
+
+
+
+# # Download municipality boundaries - TUTORIAL
+# nlCity <- raster::getData('GADM',country='NLD', level=2)
+# class(nlCity)
+# # Investigate the structure of the object
+# head(nlCity@data)
