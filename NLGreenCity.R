48d9da8e0fbe57b77d29164a400e720880b07a03
Slenders <lieven.slenders@wur.nl>
2016-01-13 15:05:21 +0100

    commit

--- NLGreenCity.R
+++ NLGreenCity.R
@@ -25,47 +25,50 @@ unzipFile('data/MODIS', 'data')
 library(sp)
 library(raster)
 MODIS <- stack("data/MOD13A3.A2014001.h18v03.005.grd")
-
+MODIS
 ## Calculations --------------------------------------------
-
-## Select Month = January on MODIS
-MODIS_month <- raster(MODIS, 1)
-# MODIS_month <- MODIS@layers[[1]]    ## Both ways work to select a column from a raster package
-# plot(MODIS_month)
-
 
 ## Import municipality mask & Apply CRS 
 # for country =NLD ;level of detail=2
 municipality_mask <- spTransform(raster::getData('GADM',country= "NLD", level= 2), CRS(proj4string(MODIS)))
 
-
 ## Extracting NDVI; calculate mean; for defined mask ---------------------------------------------
 paste(" takes some time ...")
-raw_NDVI_NL <- extract(MODIS_month, 
+
+NDVI_NL <- extract(MODIS, 
                    municipality_mask,
                    sp=TRUE,
                    df=TRUE,
                    fun=mean) ## df=TRUE i.e. return as a data.frame
 
-str(raw_NDVI_NL@data) # to check month-colum header; NDVI-name
 
-## Remove NA values ------! still month in the column header!! ???
-NDVI_NL <- raw_NDVI_NL[!is.na(raw_NDVI_NL$January),] ## remove rows with NA
-
-# answer computing --------------------------------------------------------------------------------
-
-
+## Define dataframe as variable to make easier to work with
 df <- NDVI_NL@data # has municip name as " df$NAME_2"  ; NDVI as " month name" 
 
-# xplore data
-str(NDVI_NL@data)
-df$NAME_2           # municipalityname
-df$January          # NDVI values
+## Add column for yearly average of NDVI values to dataframe
+df$yearMean <- rowMeans(df[16:27], na.rm = TRUE)
 
 
+# answer computing --------------------------------------------------------------------------------
+##  ! Still; in progress; confirm nnew deadline !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+## function to take max value of NDVI for a given time period
+findGreenestCity <- function() {
+  ## will add flow control 
+    # nered to make it more generic still
+  greenestCityJanuary <- c(df[which(df$January == max(df$January, na.rm = TRUE)), ]$NAME_2,
+                                     df[which(df$January == max(df$January, na.rm = TRUE)), ]$January)
+  greenestCityAugust <- c(df[which(df$August == max(df$August, na.rm = TRUE)), ]$NAME_2,
+                                     df[which(df$August == max(df$August, na.rm = TRUE)), ]$August)
+  greenestCityYear <- c(df[which(df$yearMean == max(df$yearMean, na.rm = TRUE)), ]$NAME_2,
+                                    df[which(df$yearMean == max(df$yearMean, na.rm = TRUE)), ]$yearMean)
+  return(c(greenestCityJanuary,greenestCityAugust,greenestCityYear))
+  
+  ## with correct IF- requisite it will plot the outcome
+  # in order to plot first match df return to NDVI_NL
+  # plot()
+}
 
-# select max NDVI ;  and display corresponding Municipality name
-answer <- df[which(df$January == max(df$January, na.rm = TRUE)), ]$NAME_2
-answer
+
+findGreenestCity()
 
 
